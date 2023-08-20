277f8d0a7540874620e2aa12290853a0d5be8c33
Rui Zhu <rui.zhu@ibm.com>
2023-08-20 15:23:02 -0400

    add: textbox component

--- apps/web/app/(author)/components/AuthorLayout.tsx
+++ apps/web/app/(author)/components/AuthorLayout.tsx
@@ -1,9 +1,21 @@
+import DynamicTextBoxContainer from './DynamicTextBoxContainer';
+import TextBox from "./TextBox";
+
+
 interface Props {}
 
+
 function AuthorLayout(props: Props) {
-  const {} = props;
 
-  return <div className="flex justify-center h-full my-auto">AuthorLayout</div>;
+  return (
+    <div className="bg-white flex flex-col min-h-screen">
+      <div className="flex-grow">
+        <TextBox />
+      </div>
+      <div className="mt-auto">
+        <DynamicTextBoxContainer />
+      </div>
+    </div>
+  );
 }
-
 export default AuthorLayout;
