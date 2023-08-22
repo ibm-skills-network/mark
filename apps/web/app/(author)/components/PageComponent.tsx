01142267d198859c09816710122a2e8856bdfe97
Rui Zhu <rui.zhu@ibm.com>
2023-08-22 15:53:19 -0400

    add: bar on buttons

--- apps/web/app/(author)/components/PageComponent.tsx
+++ apps/web/app/(author)/components/PageComponent.tsx
@@ -17,30 +17,37 @@ function PageComponent({ children }) {
           className="text-blue-700 font-bold mr-2"
           style={{ lineHeight: "1.5rem", textAlign: "left" }}
         >
+          <div className={`w-199.25 h-1 ${currentPage === 1 ? 'bg-blue-700' : 'bg-gray-300'}`}></div>
           Step: <span className="text-blue-700 font-normal">1</span>
           <br />
-          <span className="text-black font-normal">Set Up Intro</span>
+          <span className={`font-normal ${currentPage !== 1 ? 'text-gray-700' : 'text-black'}`}>Set Up Intro</span>
         </button>
+
+
 
           <button
             onClick={() => handlePageChange(2)}
             className="text-blue-700 font-bold mr-2"
             style={{ lineHeight: "1.5rem", textAlign: "left" }}
           >
-            Step: <span className="font-normal">2</span>
+            <div className={`w-199.25 h-1 ${currentPage === 2 ? 'bg-blue-700' : 'bg-gray-300'}`}></div>
+            Step: <span className="text-blue-700 font-normal">2</span>
             <br />
-            <span className="text-black font-normal">Questions and Rubrics</span>
+            <span className={`font-normal ${currentPage !== 2 ? 'text-gray-700' : 'text-black'}`}>Questions and Rubrics</span>
           </button>
+
+
           <button
             onClick={() => handlePageChange(3)}
-            className="text-blue-700 font-bold"
+            className="text-blue-700 font-bold mr-2"
             style={{ lineHeight: "1.5rem", textAlign: "left" }}
           >
-            
-            Step: <span className="font-normal">3</span>
+            <div className={`w-199.25 h-1 ${currentPage === 3 ? 'bg-blue-700' : 'bg-gray-300'}`}></div>
+            Step: <span className="text-blue-700 font-normal">3</span>
             <br />
-            <span className="text-black font-normal">Preview</span>
+            <span className={`font-normal ${currentPage !== 3 ? 'text-gray-700' : 'text-black'}`}>Preview</span>
           </button>
+
         </div>
         <div className="mt-0 flex-grow">{children(currentPage)}</div>
       </div>
