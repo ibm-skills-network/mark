5eef8b3bbcb162e8bb0b2cfdefdb48503c3f8741
Rui Zhu <rui.zhu@ibm.com>
2023-08-22 15:35:40 -0400

    add: upload file for author

--- apps/web/app/(author)/components/AuthorIntroduction.tsx
+++ apps/web/app/(author)/components/AuthorIntroduction.tsx
@@ -2,13 +2,17 @@
 "use client";
 
 import MarkdownEditor from "@components/MarkDownEditor";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+
 
 const AuthorIntroduction = () => {
   const [introduction, setIntroduction] = useState("");
   const [instruction, setInstruction] = useState("");
   const [isGraded, setIsGraded] = useState(true); 
   const [selectedAttempt, setSelectedAttempt] = useState(1); // Default selected attempt is 1
+
+  const [file, setFile] = useState()
+
   const handleAttemptChange = (event) => {
     setSelectedAttempt(parseInt(event.target.value));
   };
@@ -28,6 +32,13 @@ const AuthorIntroduction = () => {
   const handleGradedChange = () => {
     setIsGraded(true); // Set isActive to false when the component loses focus
   };
+
+
+
+  function handleChange(event) {
+    setFile(event.target.files[0])
+  }
+
 
   return (
 
@@ -185,8 +196,24 @@ const AuthorIntroduction = () => {
 
 
 
+      <button
+        className="bg-white border text-black p-2 rounded-md"
+        onClick={handleAttemptChange}
+        style={{
+          width: "19.125rem",
+          height: "3.5rem",
+        }}
+      >
+        <span style={{ marginLeft: "10px" }}>
+          {"Attempts"}
+        </span>
+      </button>
 
-      <button id="dropdownDividerButton" data-dropdown-toggle="dropdownDivider" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown divider <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+
+
+
+
+      <button id="dropdownDividerButton" data-dropdown-toggle="dropdownDivider" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Attempts <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
       </svg></button>
 
@@ -257,7 +284,10 @@ const AuthorIntroduction = () => {
 
 
       <div className="w-[1082px] h-[104px] bg-gray-50 rounded-tl-xl rounded-tr-xl border -mt-20 border-gray-300" />
-        <></>
+      <form>
+          <input type="file" onChange={handleChange}/>
+          <button type="submit">Upload</button>
+        </form>
       </div>
 
 
--- apps/web/app/(author)/components/PageComponent.tsx
+++ apps/web/app/(author)/components/PageComponent.tsx
@@ -12,15 +12,16 @@ function PageComponent({ children }) {
     <div className="bg-white min-h-screen">
       <div className="bg-white flex flex-col min-h-screen">
         <div className="flex items-center justify-between mt-4">
-          <button
-            onClick={() => handlePageChange(1)}
-            className="text-blue-700 font-bold mr-2"
-            style={{ lineHeight: "1.5rem", textAlign: "left" }}
-          >
-            Step: <span className="font-normal">1</span>
-            <br />
-            Set Up Intro
-          </button>
+        <button
+          onClick={() => handlePageChange(1)}
+          className="text-blue-700 font-bold mr-2"
+          style={{ lineHeight: "1.5rem", textAlign: "left" }}
+        >
+          Step: <span className="text-blue-700 font-normal">1</span>
+          <br />
+          <span className="text-black font-normal">Set Up Intro</span>
+        </button>
+
           <button
             onClick={() => handlePageChange(2)}
             className="text-blue-700 font-bold mr-2"
@@ -28,16 +29,17 @@ function PageComponent({ children }) {
           >
             Step: <span className="font-normal">2</span>
             <br />
-            Questions and Rubrics
+            <span className="text-black font-normal">Questions and Rubrics</span>
           </button>
           <button
             onClick={() => handlePageChange(3)}
             className="text-blue-700 font-bold"
             style={{ lineHeight: "1.5rem", textAlign: "left" }}
           >
+            
             Step: <span className="font-normal">3</span>
             <br />
-            Preview
+            <span className="text-black font-normal">Preview</span>
           </button>
         </div>
         <div className="mt-0 flex-grow">{children(currentPage)}</div>
