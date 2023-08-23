50336e1dd6eb2f255d28c1a56010ac70670524af
Benny Li <Benny.Li@ibm.com>
2023-08-22 18:12:49 -0700

    refine logic on MC

--- apps/web/app/(learner)/components/LongFormQuestion.tsx
+++ apps/web/app/(learner)/components/LongFormQuestion.tsx
@@ -1,32 +1,3 @@
-/**
- * LongFormQuestion Component Design Document
- *
- * Overview:
- * The LongFormQuestion component is designed to present long-form questions where users can type out their answers.
- * The component includes the question text, instructions, a text area for the user's response, and a maximum word count.
- *
- * Component Structure:
- * 1. Title: Displays the question's title and the points for the question.
- * 2. Instructions: Provides additional instructions for answering the question.
- * 3. Text Area: Allows users to input their answers.
- * 4. Word Count: Displays the maximum word count for the answer.
- * 5. Navigation Buttons: "Back to Instructions" and "Submit Question" buttons for navigation.
- *
- * Props:
- * - questionText (string): The text of the question to be displayed.
- * - instructions (string): Additional instructions or guidelines for the question.
- * - maxWords (number, optional): The maximum number of words allowed in the answer. Defaults to 800.
- * - points (number): Points allocated for the question.
- *
- * State:
- * - answer (string): Keeps track of the user's typed answer.
- *
- * Conclusion:
- * The LongFormQuestion component offers a straightforward way to present long-form questions, suitable for use in exams,
- * assignments, and various educational applications. It is designed for ease of integration with overview components
- * and provides flexibility for various use cases.
- */
-
 "use client";
 
 import MarkdownEditor from "@components/MarkDownEditor";
