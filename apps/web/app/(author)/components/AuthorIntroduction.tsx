4e8448468ff683c3cfaa830fc1199f786fe48b2d
Rui Zhu <rui.zhu@ibm.com>
2023-08-23 10:27:17 -0400

    feat: upload drag and drop

--- apps/web/app/(author)/components/AuthorIntroduction.tsx
+++ apps/web/app/(author)/components/AuthorIntroduction.tsx
@@ -12,29 +12,84 @@ const AuthorIntroduction = () => {
 
   const [file, setFile] = useState();
 
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////Implement the drag and drop for upload/////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+  const [dragActive, setDragActive] = React.useState(false);
+  // ref
+  const inputRef = React.useRef(null);
+  
+  // handle drag events
+  const handleDrag = function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.type === "dragenter" || e.type === "dragover") {
+      setDragActive(true);
+    } else if (e.type === "dragleave") {
+      setDragActive(false);
+    }
+  };
+  
+  // triggers when file is dropped
+  const handleDrop = function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(false);
+    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+      // handleFiles(e.dataTransfer.files);
+    }
+  };
+  
+  // triggers when file is selected with click
+  const handleChange = function(e) {
+    e.preventDefault();
+    if (e.target.files && e.target.files[0]) {
+      // handleFiles(e.target.files);
+    }
+  };
+  
+// triggers the input when the button is clicked
+  const onButtonClick = () => {
+    inputRef.current.click();
+  };
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+  // this handles how many attempt the learner can take the assignment
   const handleAttemptChange = (event) => {
     setSelectedAttempt(parseInt(event.target.value));
   };
 
+  // this handles the introduction markdown input
   const handleIntroductionChange = (newValue) => {
     setIntroduction(newValue);
   };
 
+  // this handles the instruction markdown input
   const handleInstructionChange = (newValue) => {
     setInstruction(newValue);
   };
 
+  // this handles when user set this assignment to be practice/ungraded
   const handleUngradedChange = () => {
-    setIsGraded(false); // Set isActive to true when the component is focused
+    setIsGraded(false); 
   };
-
+  // this handles when user set this assignment to be graded
   const handleGradedChange = () => {
-    setIsGraded(true); // Set isActive to false when the component loses focus
+    setIsGraded(true);
   };
 
-  function handleChange(event) {
-    setFile(event.target.files[0]);
-  }
 
   return (
     <div>
@@ -369,10 +424,19 @@ const AuthorIntroduction = () => {
           Drag and drop any files the student may need to interact
         </div>
 
-        <div className="w-[1082px] h-[104px] bg-gray-50 rounded-tl-xl rounded-tr-xl border -mt-20 border-gray-300" />
-        <form>
-          <input type="file" onChange={handleChange} />
-          <button type="submit">Upload</button>
+
+
+
+        
+        <form id="form-file-upload" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
+          <input ref={inputRef} type="file" id="input-file-upload" multiple={true} onChange={handleChange} />
+          <label id="label-file-upload" htmlFor="input-file-upload" className={dragActive ? "drag-active" : "" }>
+            <div>
+              <button className="upload-button" onClick={onButtonClick}>Upload a file here or</button>
+              <p>Drag and drop</p>
+            </div> 
+          </label>
+          { dragActive && <div id="drag-file-element" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}></div> }
         </form>
       </div>
     </div>
