732b4ab68f64b9d62262932a0c5b1b2eb9c90465
Rui Zhu <rui.zhu@ibm.com>
2023-08-28 14:49:16 -0400

    seperate dropdown(question type menu) to an independent component

--- apps/web/app/author/(components)/Textbox.tsx
+++ apps/web/app/author/(components)/Textbox.tsx
@@ -2,6 +2,7 @@
 
 import MultipleChoiceQuestion from "@/app/learner/(components)/MultipleChoiceQuestion";
 import TextQuestion from "@/app/learner/(components)/TextQuestion";
+import Dropdown from "@authorComponents/Dropdown";
 import MarkdownEditor from "@components/MarkDownEditor";
 import { Listbox, RadioGroup, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronDownIcon } from "@heroicons/react/solid";
@@ -98,7 +99,7 @@ function TextBox() {
   const [isActive, setIsActive] = useState(false); // Track the active state of the component
   const [score, setScore] = useState("");
   const [switchState, setSwitchState] = useState("a");
-  const [answerTypeSelected, setanswerTypeSelected] = useState(answerTypes[0]); // use this the change the state of the answer type (single multiple written)
+  const [answerTypeSelected, setAnswerTypeSelected] = useState(answerTypes[0]); // use this the change the state of the answer type (single multiple written)
 
   useEffect(() => {
     const handleOutsideClick = (event: MouseEvent) => {
@@ -292,101 +293,11 @@ function TextBox() {
             How would you like your question type
           </div>
           <div className="my-[20px]">
-            <Listbox
-              value={answerTypeSelected}
-              onChange={setanswerTypeSelected}
-            >
-              {({ open }) => (
-                <>
-                  <Listbox.Label className="sr-only">
-                    Change published status
-                  </Listbox.Label>
-                  <div className="relative">
-                    <div className="inline-flex divide-x divide-indigo-700 rounded-md shadow-sm">
-                      <div className="inline-flex items-center gap-x-1.5 rounded-l-md bg-indigo-600 px-3 py-2 text-white shadow-sm">
-                        <CheckIcon
-                          className="-ml-0.5 h-5 w-5"
-                          aria-hidden="true"
-                        />
-                        <p className="text-sm font-semibold">
-                          {answerTypeSelected.title}
-                        </p>
-                      </div>
-                      <Listbox.Button className="inline-flex items-center rounded-l-none rounded-r-md bg-indigo-600 p-2 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 focus:ring-offset-gray-50">
-                        <span className="sr-only">Change published status</span>
-                        <ChevronDownIcon
-                          className="h-5 w-5 text-white"
-                          aria-hidden="true"
-                        />
-                      </Listbox.Button>
-                    </div>
-
-                    <Transition
-                      show={open}
-                      as={Fragment}
-                      leave="transition ease-in duration-100"
-                      leaveFrom="opacity-100"
-                      leaveTo="opacity-0"
-                    >
-                      <Listbox.Options className="absolute right-0 z-10 mt-2 w-72 origin-top-right divide-y divide-gray-200 overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        {answerTypes.map((option) => (
-                          <Listbox.Option
-                            key={option.title}
-                            className={({ active }) =>
-                              classNames(
-                                active
-                                  ? "bg-indigo-600 text-white"
-                                  : "text-gray-900",
-                                "cursor-default select-none p-4 text-sm"
-                              )
-                            }
-                            value={option}
-                          >
-                            {({ selected, active }) => (
-                              <div className="flex flex-col">
-                                <div className="flex justify-between">
-                                  <p
-                                    className={
-                                      selected ? "font-semibold" : "font-normal"
-                                    }
-                                  >
-                                    {option.title}
-                                  </p>
-                                  {selected ? (
-                                    <span
-                                      className={
-                                        active
-                                          ? "text-white"
-                                          : "text-indigo-600"
-                                      }
-                                    >
-                                      <CheckIcon
-                                        className="h-5 w-5"
-                                        aria-hidden="true"
-                                      />
-                                    </span>
-                                  ) : null}
-                                </div>
-                                <p
-                                  className={classNames(
-                                    active
-                                      ? "text-indigo-200"
-                                      : "text-gray-500",
-                                    "mt-2"
-                                  )}
-                                >
-                                  {option.description}
-                                </p>
-                              </div>
-                            )}
-                          </Listbox.Option>
-                        ))}
-                      </Listbox.Options>
-                    </Transition>
-                  </div>
-                </>
-              )}
-            </Listbox>
+            <Dropdown
+              answerTypeSelected={answerTypeSelected}
+              setAnswerTypeSelected={setAnswerTypeSelected}
+              answerTypes={answerTypes}
+            />
           </div>
           {answerTypeSelected === answerTypes[0] ? (
             <div className="mt-4">
