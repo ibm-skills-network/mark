bdd369eca47ba7ea82acab15054f812e87a7991a
Rui Zhu <rui.zhu@ibm.com>
2023-08-29 21:48:19 -0400

    seperate written question in to writtenquestionview component for author

--- apps/web/app/author/(components)/Textbox.tsx
+++ apps/web/app/author/(components)/Textbox.tsx
@@ -9,6 +9,7 @@ import { CheckIcon, ChevronDownIcon } from "@heroicons/react/solid";
 import React, { Fragment, useEffect, useRef, useState } from "react";
 import MultipleAnswerSection from "./MultipleAnswerSection";
 import RubricTable from "./RubricTable";
+import WrittenQuestionView from "./WrittenQuestionView";
 
 //////////////////Answer Type////////////////////////////////////////
 const answerTypes = [
@@ -23,7 +24,7 @@ const answerTypes = [
     current: false,
   },
   {
-    title: "Written Answer Question",
+    title: "Text",
     description: "This question has a written answer",
     current: false,
   },
@@ -272,7 +273,7 @@ function TextBox() {
             }}
           >
             <MarkdownEditor
-              style={{ height: "200px", width: "800px" }}
+              style={{ height: "150px", width: "1200px" }}
               value={inputText}
               setValue={setInputText}
             />
@@ -396,129 +397,7 @@ function TextBox() {
               }
             />
           ) : null}
-          {answerTypeSelected === answerTypes[2] && (
-            <div className="mt-4">
-              <div style={{ width: 800, height: 104, background: "#E0E7FF" }}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="65"
-                  height="66"
-                  viewBox="0 0 65 66"
-                  fill="none"
-                  style={{ transform: "translate(10px, 15px)" }} // Adjust the values as needed
-                >
-                  <path
-                    d="M24.4342 32.5332H34.4685M24.4342 40.6582H34.4685M24.4342 48.7832H34.4685M42.496 50.8145H48.5166C50.1134 50.8145 51.6447 50.1724 52.7738 49.0296C53.9029 47.8868 54.5372 46.3369 54.5372 44.7207V16.5757C54.5372 13.5017 52.2762 10.8936 49.2498 10.639C48.249 10.555 47.2473 10.4828 46.2448 10.4224M46.2448 10.4224C46.4224 11.0049 46.5099 11.6111 46.5098 12.2207C46.5098 12.7594 46.2983 13.2761 45.922 13.657C45.5456 14.0379 45.0351 14.252 44.5029 14.252H32.4617C31.3539 14.252 30.4548 13.342 30.4548 12.2207C30.4548 11.5951 30.5485 10.9911 30.7224 10.4224M46.2448 10.4224C45.4876 7.93612 43.1971 6.12695 40.4892 6.12695H36.4754C35.189 6.12726 33.9364 6.54443 32.9012 7.31737C31.866 8.09032 31.1024 9.17843 30.7224 10.4224M30.7224 10.4224C29.7163 10.4847 28.7155 10.5578 27.7148 10.639C24.6884 10.8936 22.4273 13.5017 22.4273 16.5757V22.377M22.4273 22.377H13.3964C11.7348 22.377 10.3861 23.742 10.3861 25.4238V55.8926C10.3861 57.5745 11.7348 58.9395 13.3964 58.9395H39.4857C41.1474 58.9395 42.496 57.5745 42.496 55.8926V25.4238C42.496 23.742 41.1474 22.377 39.4857 22.377H22.4273ZM18.4136 32.5332H18.435V32.5549H18.4136V32.5332ZM18.4136 40.6582H18.435V40.6799H18.4136V40.6582ZM18.4136 48.7832H18.435V48.8049H18.4136V48.7832Z"
-                    stroke="#1D4ED8"
-                    stroke-width="2.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-                <div
-                  style={{
-                    width: 223.17,
-                    color: "#1D4ED8",
-                    fontSize: 19.17,
-                    fontWeight: "bold",
-                    transform: "translate(5px, -45px)", // Adjust the vertical value as needed
-                    textAlign: "center",
-                  }}
-                >
-                  Rubric
-                </div>
-              </div>
-              <p>Points:</p>
-              <input
-                type="number"
-                className="p-2 border rounded-md text-gray-700 bg-transparent outline-none"
-                placeholder={`ex. 10`}
-                value={score}
-                onChange={handleScore}
-                min={1}
-                max={100}
-                style={{
-                  maxWidth: "100%",
-                }}
-              />
-              <div className="flex items-center">
-                <label className="switch">
-                  <button
-                    className={`text-black bg-white hover:bg-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-white dark:hover:bg-gray-300 ${
-                      switchState === "a" ? "bg-blue-800" : ""
-                    }`}
-                    onClick={() => setSwitchState("a")}
-                  >
-                    Single Criteria (easy mode)
-                  </button>
-
-                  <button
-                    className={`text-black bg-white hover:bg-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-white dark:hover:bg-gray-300 ${
-                      switchState === "b" ? "bg-blue-800" : ""
-                    }`}
-                    onClick={() => setSwitchState("b")}
-                  >
-                    Multiple Criteria (Pro Mode)
-                  </button>
-
-                  <span className="slider round"></span>
-                </label>
-              </div>
-
-              {switchState === "a" && (
-                <div>
-                  <p>Point distribution for the Rubric</p>
-                  {optionsWrittenQuestion.map((option, index) => (
-                    <div key={index} className="flex items-center">
-                      <input
-                        type="text"
-                        className="p-2 rounded-md text-black bg-transparent outline-none w-full" // Removed 'border ml-2' and added 'w-full'
-                        placeholder={`Option ${index + 1}`}
-                        value={option}
-                        onChange={(event) =>
-                          handleOptionChangeWrittenQuestion(
-                            index,
-                            event.target.value
-                          )
-                        }
-                        style={{
-                          height: "2.125rem",
-                          maxWidth: "100%",
-                        }}
-                      />
-                      <button
-                        className="ml-2 text-red-600"
-                        onClick={() => handleRemoveOptionWrittenQuestion(index)}
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke-width="1.5"
-                          stroke="currentColor"
-                          className="w-6 h-6"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M12 9.75L14.25 12m0 0l2.25 2.25M14.25 12l2.25-2.25M14.25 12L12 14.25m-2.58 4.92l-6.375-6.375a1.125 1.125 0 010-1.59L9.42 4.83c.211-.211.498-.33.796-.33H19.5a2.25 2.25 0 012.25 2.25v10.5a2.25 2.25 0 01-2.25 2.25h-9.284c-.298 0-.585-.119-.796-.33z"
-                          />
-                        </svg>
-                      </button>
-                    </div>
-                  ))}
-                  <button
-                    className="bg-blue-500 text-white p-2 rounded-md mt-2"
-                    onClick={handleAddOptionWrittenQuestion}
-                  >
-                    Add Option
-                  </button>
-                </div>
-              )}
-
-              {switchState === "b" && <RubricTable rubrics={rubrics} />}
-            </div>
-          )}
+          {answerTypeSelected === answerTypes[2] && <WrittenQuestionView />}
         </div>
       )}
     </div>
