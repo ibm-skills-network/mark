f44d4a9a2ba6bf229e321a79182fa95e65ebe2ee
Talha Siddiqui <talha.siddiqui@ibm.com>
2023-09-26 11:59:23 -0400

    fix: removed hardcoded www.example.com

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -1,8 +1,7 @@
 export function absoluteUrl(path: string) {
   const base =
     process.env.NODE_ENV === "production"
-      ? // TODO: change this to the actual URL
-        "https://www.example.com"
+      ? process.env.NEXT_PUBLIC_APP_URL || ""
       : process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
   return `${base}${path}`;
 }
