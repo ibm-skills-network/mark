2e2ad5255cec083e8e10fbad12efec1175fdecad
Rami Maalouf <rami.maalouf@ibm.com>
2023-10-26 11:23:12 -0400

    spinner is now it's own component so it can be used in other places

--- apps/web/components/Loading.tsx
+++ apps/web/components/Loading.tsx
@@ -1,4 +1,5 @@
 import { type ComponentPropsWithoutRef } from "react";
+import Spinner from "./svgs/Spinner";
 
 interface Props extends ComponentPropsWithoutRef<"div"> {}
 
@@ -8,20 +9,7 @@ function Loading(props: Props) {
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="flex justify-center items-center gap-x-1 text-sm text-gray-700">
-        <svg
-          fill="none"
-          className="w-6 h-6 animate-spin"
-          viewBox="0 0 32 32"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            clipRule="evenodd"
-            d="M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z"
-            fill="currentColor"
-            fillRule="evenodd"
-          />
-        </svg>
-
+        <Spinner className="" />
         <div>Loading ...</div>
       </div>
     </div>
