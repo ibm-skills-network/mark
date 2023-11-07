7a333ce6e1e99eef1efa055b1b2b2e60cbd30ed6
Talha Siddiqui <talha.siddiqui@ibm.com>
2023-11-07 15:45:41 -0500

    feat: added health check for database

--- apps/api/src/health/health.service.ts
+++ apps/api/src/health/health.service.ts
@@ -3,13 +3,17 @@ import {
   DiskHealthIndicator,
   HealthCheckResult,
   HealthCheckService,
+  PrismaHealthIndicator,
 } from "@nestjs/terminus";
+import { PrismaService } from "../prisma.service";
 
 @Injectable()
 export class HealthService {
   constructor(
     private readonly health: HealthCheckService,
-    private readonly disk: DiskHealthIndicator
+    private readonly disk: DiskHealthIndicator,
+    private readonly prisma: PrismaHealthIndicator,
+    private readonly prismaService: PrismaService
   ) {}
 
   checkReadiness(): Promise<HealthCheckResult> {
@@ -20,6 +24,7 @@ export class HealthService {
     return this.health.check([
       () =>
         this.disk.checkStorage("storage", { path: "/", thresholdPercent: 0.9 }),
+      () => this.prisma.pingCheck("database", this.prismaService),
     ]);
   }
 }
