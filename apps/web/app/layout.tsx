5136f800562d65067b3f12a04dbf340f006f6208
Rami Maalouf <rami.maalouf@ibm.com>
2023-11-22 14:42:32 -0500

    added metadata

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -9,6 +9,27 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Mark",
   description: "Grade your learners' work with the power of AI.",
+  icons: {
+    icon: "favicon.ico",
+  },
+  keywords: [
+    "mark",
+    "skills network",
+    "ai",
+    "AI graded assignments",
+    "online learning",
+    "online courses",
+  ],
+  authors: [
+    {
+      name: "Skills Network",
+      url: "https://skills.network",
+    },
+    {
+      name: "Rami Maalouf",
+      url: "https://ramimaalouf.tech",
+    },
+  ],
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
