6ec9a51ceff684538df963ed18e7549e3ad33c5f
Rami Maalouf <rami.maalouf@ibm.com>
2023-12-07 11:09:19 -0500

    fix: cursor focus on next choice when you create a new choice

--- apps/web/app/author/(components)/Questions/QuestionTypes/Choice.tsx
+++ apps/web/app/author/(components)/Questions/QuestionTypes/Choice.tsx
@@ -1,5 +1,5 @@
 import { XMarkIcon } from "@heroicons/react/24/outline";
-import { type ComponentPropsWithoutRef, type ChangeEvent } from "react";
+import { type ChangeEvent, type ComponentPropsWithoutRef } from "react";
 
 interface Props extends ComponentPropsWithoutRef<"li"> {
   index: number;
@@ -8,11 +8,19 @@ interface Props extends ComponentPropsWithoutRef<"li"> {
   toggleChoice: (choiceIndex: number) => void;
   modifyChoice: (index: number, value: string) => void;
   removeChoice: (choiceIndex: number) => void;
+  addChoice: () => void;
 }
 
 function Component(props: Props) {
-  const { index, choice, isChecked, toggleChoice, modifyChoice, removeChoice } =
-    props;
+  const {
+    index,
+    choice,
+    isChecked,
+    toggleChoice,
+    modifyChoice,
+    removeChoice,
+    addChoice,
+  } = props;
 
   function handleChoiceTextChange(event: ChangeEvent<HTMLInputElement>) {
     modifyChoice(index, event.target.value);
@@ -32,6 +40,28 @@ function Component(props: Props) {
         placeholder={`Choice ${index + 1}`}
         value={choice}
         onChange={handleChoiceTextChange}
+        onKeyDown={(event) => {
+          if (event.key === "Enter") {
+            addChoice();
+
+
+            // Hacky vanilla javascript to get the reference of the next input (first get the reference to the parent element, then get the next sibling,
+            // then get the first child, then get the next sibling)
+            // wait 100ms for the next input to be rendered, then focus on it
+            setTimeout(() => {
+              const inputElement = event.target as HTMLInputElement;
+              const nextInput = inputElement.parentElement?.nextSibling
+                ?.firstChild?.nextSibling as HTMLInputElement;
+              if (nextInput) {
+                nextInput.focus();
+              }
+            }, 100);
+          }
+          // if user clicks backspace and the input field is empty, delete the choice
+          if (event.key === "Backspace" && choice === "") {
+            removeChoice(index);
+          }
+        }}
       />
       <button
         className=" text-red-600"
