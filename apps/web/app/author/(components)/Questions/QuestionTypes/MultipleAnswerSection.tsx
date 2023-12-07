a0ae522c7bb3a4d026e99ab33ec5b16a0276e1ba
Rami Maalouf <rami.maalouf@ibm.com>
2023-12-07 11:12:52 -0500

    Suggest to the user to click enter to create a new choice in a multiple choice Q

--- apps/web/app/author/(components)/Questions/QuestionTypes/MultipleAnswerSection.tsx
+++ apps/web/app/author/(components)/Questions/QuestionTypes/MultipleAnswerSection.tsx
@@ -5,6 +5,7 @@ import { useAutoAnimate } from "@formkit/auto-animate/react";
 import React, { useEffect, useState, type ChangeEvent } from "react";
 import QuestionNumberOfRetries from "../../QuestionNumberOfRetries";
 import Choice from "./Choice";
+import {toast } from "sonner";
 
 interface sectionProps {
   questionId: number;
@@ -33,6 +34,8 @@ function Section(props: sectionProps) {
     state.setPoints,
   ]);
 
+  const numOfTimeSuggestionHasBeenShown = ~~localStorage.getItem('numOfTimeSuggestionHasBeenShown') || 0;
+
   const question = questions.find((question) => question.id === questionId);
   const { choices, totalPoints: points, numRetries: retries } = question;
   useEffect(() => {
@@ -43,7 +46,7 @@ function Section(props: sectionProps) {
           choice: "",
           isCorrect: false,
           points: 1,
-        }
+        },
       ]);
     }
   }, []);
@@ -123,41 +126,48 @@ function Section(props: sectionProps) {
             modifyChoice={handleChoiceChange}
             toggleChoice={handleChoiceToggle}
             removeChoice={handleRemoveChoice}
+            addChoice={handleAddChoice}
           />
         ))}
       </ul>
-      <div className="">
-        <button
-          type="button"
-          disabled={disableAddChoice}
-          className="rounded-full bg-white px-5 py-2.5 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-200 disabled:opacity-50 transition"
-          onClick={handleAddChoice}
+      <button
+        type="button"
+        disabled={disableAddChoice}
+        className="flex mr-auto rounded-full bg-white px-5 py-2.5 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-200 disabled:opacity-50 transition"
+        onClick={() => {
+          handleAddChoice();
+          if (numOfTimeSuggestionHasBeenShown < 3) {
+            toast.message("You can also press enter to create a new choice!", {
+              position: "bottom-center",
+              duration: 5000,
+              important: true,
+            });
+            localStorage.setItem('numOfTimeSuggestionHasBeenShown', (numOfTimeSuggestionHasBeenShown + 1).toString());
+            }
+          }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="21"
+          viewBox="0 0 20 21"
+          fill="none"
         >
-          <div className="flex items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="21"
-              viewBox="0 0 20 21"
-              fill="none"
-            >
-              <path
-                d="M11.3438 7.34375C11.3438 7.11997 11.2549 6.90536 11.0966 6.74713C10.9384 6.58889 10.7238 6.5 10.5 6.5C10.2762 6.5 10.0616 6.58889 9.90338 6.74713C9.74515 6.90536 9.65625 7.11997 9.65625 7.34375V10.1562H6.84375C6.61997 10.1562 6.40536 10.2451 6.24713 10.4034C6.08889 10.5616 6 10.7762 6 11C6 11.2238 6.08889 11.4384 6.24713 11.5966C6.40536 11.7549 6.61997 11.8438 6.84375 11.8438H9.65625V14.6562C9.65625 14.88 9.74515 15.0946 9.90338 15.2529C10.0616 15.4111 10.2762 15.5 10.5 15.5C10.7238 15.5 10.9384 15.4111 11.0966 15.2529C11.2549 15.0946 11.3438 14.88 11.3438 14.6562V11.8438H14.1562C14.38 11.8438 14.5946 11.7549 14.7529 11.5966C14.9111 11.4384 15 11.2238 15 11C15 10.7762 14.9111 10.5616 14.7529 10.4034C14.5946 10.2451 14.38 10.1562 14.1562 10.1562H11.3438V7.34375Z"
-                fill="#1D4ED8"
-              />
-            </svg>
-            <span
-              style={{
-                fontSize: "0.8rem",
-                whiteSpace: "nowrap", // Prevent text from wrapping
-                display: "inline-block", // Ensure it stays on one line
-              }}
-            >
-              Add Option
-            </span>
-          </div>
-        </button>
-      </div>
+          <path
+            d="M11.3438 7.34375C11.3438 7.11997 11.2549 6.90536 11.0966 6.74713C10.9384 6.58889 10.7238 6.5 10.5 6.5C10.2762 6.5 10.0616 6.58889 9.90338 6.74713C9.74515 6.90536 9.65625 7.11997 9.65625 7.34375V10.1562H6.84375C6.61997 10.1562 6.40536 10.2451 6.24713 10.4034C6.08889 10.5616 6 10.7762 6 11C6 11.2238 6.08889 11.4384 6.24713 11.5966C6.40536 11.7549 6.61997 11.8438 6.84375 11.8438H9.65625V14.6562C9.65625 14.88 9.74515 15.0946 9.90338 15.2529C10.0616 15.4111 10.2762 15.5 10.5 15.5C10.7238 15.5 10.9384 15.4111 11.0966 15.2529C11.2549 15.0946 11.3438 14.88 11.3438 14.6562V11.8438H14.1562C14.38 11.8438 14.5946 11.7549 14.7529 11.5966C14.9111 11.4384 15 11.2238 15 11C15 10.7762 14.9111 10.5616 14.7529 10.4034C14.5946 10.2451 14.38 10.1562 14.1562 10.1562H11.3438V7.34375Z"
+            fill="#1D4ED8"
+          />
+        </svg>
+        <span
+          style={{
+            fontSize: "0.8rem",
+            whiteSpace: "nowrap", // Prevent text from wrapping
+            display: "inline-block", // Ensure it stays on one line
+          }}
+        >
+          Add Option
+        </span>
+      </button>
       {/* <div>
         <svg
           xmlns="http://www.w3.org/2000/svg"
