f4ff7fa7ea0edbb486c46d1d16e67fea1ae976d2
Rami Maalouf <rami.maalouf@ibm.com>
2023-12-12 01:24:27 +0400

    added devtools for author zustand store

--- apps/web/stores/author.ts
+++ apps/web/stores/author.ts
@@ -1,5 +1,6 @@
 import type { Choice, Criteria, QuestionAuthorStore } from "@/config/types";
 import { createRef, type RefObject } from "react";
+import { devtools } from "zustand/middleware";
 import { shallow } from "zustand/shallow";
 import { createWithEqualityFn } from "zustand/traditional";
 
@@ -36,186 +37,192 @@ export type AuthorActions = {
   setPoints: (questionId: number, points: number) => void;
 };
 
-export const useAuthorStore = createWithEqualityFn<AuthorState & AuthorActions>(
-  (set, get) => ({
-    activeAssignmentId: null,
-    setActiveAssignmentId: (id) => set({ activeAssignmentId: id }),
-    assignmentTitle: "",
-    setAssignmentTitle: (title) => set({ assignmentTitle: title }),
-    updateAssignmentButtonRef: createRef<HTMLButtonElement>(),
-    questions: [],
-    setQuestions: (questions) => set({ questions }),
-    addQuestion: (question) =>
-      set((state) => ({ questions: [...state.questions, question] })),
-    removeQuestion: (questionId) =>
-      set((state) => ({
-        questions: state.questions.filter((q) => q.id !== questionId),
-      })),
-    modifyQuestion: (questionId, modifiedData) =>
-      set((state) => ({
-        questions: state.questions.map((q) =>
-          q.id === questionId
-            ? {
-                // keep the original data
-                ...q,
-                // add the modified data to the question
-                ...modifiedData,
-              }
-            : {
-                ...q,
-              }
-        ),
-      })),
-    setCriterias: (questionId, criterias) => {
-      set((state) => ({
-        questions: state.questions.map((q) => {
-          if (q.id === questionId) {
-            return {
-              ...q,
-              scoring: {
-                ...q.scoring,
-                criteria: criterias,
-              },
-            };
-          }
-          return q;
-        }),
-      }));
-      return criterias;
-    },
-    addCriteria: (questionId, criteria) => {
-      set((state) => ({
-        questions: state.questions.map((q) => {
-          if (q.id === questionId) {
-            return {
-              ...q,
-              scoring: {
-                ...q.scoring,
-                criteria: [...q.scoring.criteria, criteria],
-              },
-            };
-          }
-          return q;
-        }),
-      }));
-    },
-    removeCriteria: (questionId, criteriaIndex) => {
-      set((state) => ({
-        questions: state.questions.map((q) => {
-          if (q.id === questionId) {
-            return {
-              ...q,
-              scoring: {
-                ...q.scoring,
-                criteria: q.scoring.criteria.filter(
-                  (_, index) => index !== criteriaIndex
-                ),
-              },
-            };
-          }
-          return q;
-        }),
-      }));
-    },
-    setChoices: (questionId, choices) => {
-      set((state) => ({
-        questions: state.questions.map((q) => {
-          if (q.id === questionId) {
-            return {
-              ...q,
-              choices,
-            };
-          }
-          return q;
-        }),
-      }));
-    },
-    addChoice: (questionId, choice) => {
-      set((state) => ({
-        questions: state.questions.map((q) => {
-          if (q.id === questionId) {
-            return {
-              ...q,
-              choices: [...q.choices, { choice, isCorrect: false, points: 0 }],
-            };
-          }
-          return q;
-        }),
-      }));
-    },
-    removeChoice: (questionId, choiceIndex) => {
-      set((state) => ({
-        questions: state.questions.map((q) => {
-          if (q.id === questionId) {
-            const choices = q.choices.filter(
-              (_, index) => index !== choiceIndex
-            );
-            return {
-              ...q,
-              choices,
-            };
-          }
-          return q;
-        }),
-      }));
-    },
-    toggleChoice: (questionId, choiceIndex) => {
-      set((state) => ({
-        questions: state.questions.map((q) => {
-          if (q.id === questionId) {
-            const choices = q.choices.map((choice, index) => {
-              if (index === choiceIndex) {
-                return {
-                  ...choice,
-                  isCorrect: !choice.isCorrect,
-                };
-              }
-              return choice;
-            });
-            return {
-              ...q,
-              choices,
-            };
-          }
-          return q;
-        }),
-      }));
-    },
-    modifyChoice: (questionId, choiceIndex, modifiedData) => {
-      set((state) => ({
-        questions: state.questions.map((q) => {
-          if (q.id === questionId) {
-            const choices = q.choices.map((choice, index) => {
-              if (index === choiceIndex) {
-                return {
-                  ...choice,
+export const useAuthorStore = createWithEqualityFn<AuthorState & AuthorActions>()(
+  devtools(
+    (set, get) => ({
+      activeAssignmentId: null,
+      setActiveAssignmentId: (id) => set({ activeAssignmentId: id }),
+      assignmentTitle: "",
+      setAssignmentTitle: (title) => set({ assignmentTitle: title }),
+      updateAssignmentButtonRef: createRef<HTMLButtonElement>(),
+      questions: [],
+      setQuestions: (questions) => set({ questions }),
+      addQuestion: (question) =>
+        set((state) => ({ questions: [...state.questions, question] })),
+      removeQuestion: (questionId) =>
+        set((state) => ({
+          questions: state.questions.filter((q) => q.id !== questionId),
+        })),
+      modifyQuestion: (questionId, modifiedData) =>
+        set((state) => ({
+          questions: state.questions.map((q) =>
+            q.id === questionId
+              ? {
+                  // keep the original data
+                  ...q,
+                  // add the modified data to the question
                   ...modifiedData,
-                };
-              }
-              return choice;
-            });
-            return {
-              ...q,
-              choices,
-            };
-          }
-          return q;
-        }),
-      }));
-    },
-    setPoints: (questionId, points) => {
-      set((state) => ({
-        questions: state.questions.map((q) => {
-          if (q.id === questionId) {
-            return {
-              ...q,
-              totalPoints: points,
-            };
-          }
-          return q;
-        }),
-      }));
-    },
-  }),
+                }
+              : {
+                  ...q,
+                }
+          ),
+        })),
+      setCriterias: (questionId, criterias) => {
+        set((state) => ({
+          questions: state.questions.map((q) => {
+            if (q.id === questionId) {
+              return {
+                ...q,
+                scoring: {
+                  ...q.scoring,
+                  criteria: criterias,
+                },
+              };
+            }
+            return q;
+          }),
+        }));
+        return criterias;
+      },
+      addCriteria: (questionId, criteria) => {
+        set((state) => ({
+          questions: state.questions.map((q) => {
+            if (q.id === questionId) {
+              return {
+                ...q,
+                scoring: {
+                  ...q.scoring,
+                  criteria: [...q.scoring.criteria, criteria],
+                },
+              };
+            }
+            return q;
+          }),
+        }));
+      },
+      removeCriteria: (questionId, criteriaIndex) => {
+        set((state) => ({
+          questions: state.questions.map((q) => {
+            if (q.id === questionId) {
+              return {
+                ...q,
+                scoring: {
+                  ...q.scoring,
+                  criteria: q.scoring.criteria.filter(
+                    (_, index) => index !== criteriaIndex
+                  ),
+                },
+              };
+            }
+            return q;
+          }),
+        }));
+      },
+      setChoices: (questionId, choices) => {
+        set((state) => ({
+          questions: state.questions.map((q) => {
+            if (q.id === questionId) {
+              return {
+                ...q,
+                choices,
+              };
+            }
+            return q;
+          }),
+        }));
+      },
+      addChoice: (questionId, choice) => {
+        set((state) => ({
+          questions: state.questions.map((q) => {
+            if (q.id === questionId) {
+              return {
+                ...q,
+                choices: [...q.choices, { choice, isCorrect: false, points: 0 }],
+              };
+            }
+            return q;
+          }),
+        }));
+      },
+      removeChoice: (questionId, choiceIndex) => {
+        set((state) => ({
+          questions: state.questions.map((q) => {
+            if (q.id === questionId) {
+              const choices = q.choices.filter(
+                (_, index) => index !== choiceIndex
+              );
+              return {
+                ...q,
+                choices,
+              };
+            }
+            return q;
+          }),
+        }));
+      },
+      toggleChoice: (questionId, choiceIndex) => {
+        set((state) => ({
+          questions: state.questions.map((q) => {
+            if (q.id === questionId) {
+              const choices = q.choices.map((choice, index) => {
+                if (index === choiceIndex) {
+                  return {
+                    ...choice,
+                    isCorrect: !choice.isCorrect,
+                  };
+                }
+                return choice;
+              });
+              return {
+                ...q,
+                choices,
+              };
+            }
+            return q;
+          }),
+        }));
+      },
+      modifyChoice: (questionId, choiceIndex, modifiedData) => {
+        set((state) => ({
+          questions: state.questions.map((q) => {
+            if (q.id === questionId) {
+              const choices = q.choices.map((choice, index) => {
+                if (index === choiceIndex) {
+                  return {
+                    ...choice,
+                    ...modifiedData,
+                  };
+                }
+                return choice;
+              });
+              return {
+                ...q,
+                choices,
+              };
+            }
+            return q;
+          }),
+        }));
+      },
+      setPoints: (questionId, points) => {
+        set((state) => ({
+          questions: state.questions.map((q) => {
+            if (q.id === questionId) {
+              return {
+                ...q,
+                totalPoints: points,
+              };
+            }
+            return q;
+          }),
+        }));
+      },
+    }),
+    {
+      name: "author",
+      enabled: process.env.NODE_ENV === "development",
+    }
+  ),
   shallow
 );
