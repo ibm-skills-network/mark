d6880ad3c07bfcbd0b501a837c8e5f2218b432da
Noah Freelove <90488068+NoahFreelove@users.noreply.github.com>
2026-04-17 15:38:43 -0400

    feat: load @instana/collector in api for distributed tracing parity with gateway

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,5 +1,9 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable unicorn/no-process-exit */
+if (process.env.NODE_ENV === "production") {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-var-requires, unicorn/prefer-module
+  require("@instana/collector")();
+}
 /**
  * Application Bootstrap File
  *
